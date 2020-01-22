ce38c9cc5b01a7990c1da4cdb338f84c0f1526b9
eggplant60 <nagoyan240@gmail.com>
2020-01-22 21:04:34 +0900

    [update] サーバサイドからの番組表取得

--- mock/controllers/main.js
+++ mock/controllers/main.js
@@ -1,21 +1,20 @@
 angular.module('app')
-.controller('AppCtrl', ['$scope', '$log', '$timeout', 'ApiService', AppCtrl]);
+	.controller('AppCtrl', ['$scope', '$log', '$timeout', 'ApiService', AppCtrl]);
 
-function AppCtrl($scope, $log, $timeout, ApiService) {
-    //var vm = this;
-    $log.debug('debug in AppCtrl()');
+function AppCtrl($scope, $log, $timeout, api) {
+	$log.debug('AppCtrl: start ---------------------');
 
+	var vm = this;
+	vm.programs = [];
 
-    ApiService.tvsearch.bytime().$promise.then(
-      (ret) => {
-        $scope.data = ret;
-        $log.debug($scope.data);
-      },
-      (httpError) => {
-        $log.error('httpError');
-        $log.error(httpError);
-      }
-    );
-    
-    
+	api.chantoru.tvsearch().$promise.then(
+		(data) => {
+			//$log.debug(data);
+			vm.programs = data;
+		},
+		(httpError) => {
+			$log.error(httpError);
+		}
+	);
+	
 }
--- mock/services/api.js
+++ mock/services/api.js
@@ -1,53 +1,49 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular.module('app')
-      .service('ApiService', ['$log', '$resource', ApiService]);
+	angular.module('app')
+			.service('ApiService', ['$log', '$resource', ApiService]);
 
-  function ApiService($log, $resource) {
+	function ApiService($log, $resource) {
 
-    var date = new Date(); // 現在時刻
+		var date = new Date(); // 現在時刻
 
-    $log.debug(date.getTime());
-    $log.debug(date.toJSON());
+		//$log.debug(date.getTime());
+		//$log.debug(date.toJSON());
 
-    var _baseUrl = 'https://tv.so-net.ne.jp/chan-toru';
-    var _url  = {
-      tvsearch : _baseUrl + '/tvsearch',
-      resv : _baseUrl + '/resv',
-    }
-    var _paramDefaults = {
-      tvsearch : {
-        op: 'bytime',
-        span: '26',
-        category: '1',
-        start: date.getTime()
-      },
-      resv : {
-        op: 'add', 
-        sid: 0, 
-        eid: 0, 
-        category: 1, 
-        date: date.toJSON(),
-        duration: 0,
-        title: '',
-        quality: 230,
-        condition: 'w1',
-        destination: 'HDD'
-      }
-    }
+		var _baseUrl = 'http://127.0.0.1:3001/api';
+		var _url  = {
+			tvsearch : _baseUrl + '/tvsearch',
+			resv : _baseUrl + '/resv',
+		};
+		var _paramDefaults = {
+			tvsearch : {
+				//op: 'bytime',
+				//span: '23',
+				//category: '1',
+				//start: date.toJSON()
+			},
+			resv : {
+				op: 'add', 
+				sid: 0, 
+				eid: 0, 
+				category: 1, 
+				date: date.toJSON(),
+				duration: 0,
+				title: '',
+				quality: 230,
+				condition: 'w1',
+				destination: 'HDD'
+			}
+		};
 
-    var tvsearch = $resource(
-      _url.tvsearch, 
-      _paramDefaults.tvsearch,
-      {
-        bytime: {method: 'POST'}
-      }
-    );
-    
-    return {
-      'tvsearch': tvsearch,
-    };
-
-  }
+		this.chantoru = $resource(
+			_url.tvsearch, 
+			_paramDefaults.tvsearch,
+			{
+				tvsearch: {method: 'GET'},
+				resv    : {method: 'GET'}
+			}
+		);
+	}
 })();
