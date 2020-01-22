17c405202cd5af0eca239fdab0f369f13edda74f
eggplant60 <nagoyan240@gmail.com>
2020-01-22 22:44:56 +0900

    [delete] 不要なファイルの削除

--- mock/directives/old/execute.js
+++
@@ -1,9 +0,0 @@
-angular.module('app')
-.directive('executeArea', executeArea);
-
-function executeArea() {
-    return {
-        restrict: 'E',
-        templateUrl: 'directives/execute.html'
-    };
-}
--- mock/directives/old/preview.js
+++
@@ -1,9 +0,0 @@
-angular.module('app')
-.directive('previewArea', previewArea);
-
-function previewArea() {
-    return {
-        restrict: 'E',
-        templateUrl: 'directives/preview.html'
-    };
-}
