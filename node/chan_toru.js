94da0f75fe18ffa43ffdbdc2b3114df4f11fe1da
eggplant60 <nagoyan240@gmail.com>
2020-02-01 19:09:29 +0900

    [fix&update] 予約一覧取得APIの実装、予約APIの修正

--- node/chan_toru.js
+++ node/chan_toru.js
@@ -15,13 +15,15 @@ var clientMethods = {
 	GET  : rest.getPromise,
 	POST : rest.postPromise,   
 };
+const headerPath = '../conf/chan_toru.json';
 
 // EXPRESS関連
 const SERVER_PORT = 3001;
 const app = Express();
 const OWN_ENDPOINT = {
-	tvsearch: '/api/tvsearch/',
-	resv: '/api/resv/'
+	tvsearch : '/api/tvsearch/',
+	resv     : '/api/resv/',
+	schedule : '/api/schedule/' ,
 };
 app.use((req, res, next) => {   // CORS対応
 	res.header('Access-Control-Allow-Origin', '*');
@@ -34,46 +36,50 @@ app.use((req, res, next) => {   // CORS対応
 		'GET, POST, PUT, DELETE, OPTIONS');
 	next();
 });
-const url  = {
+const orgUrl  = {
 	tvsearch : CHANTORU_ENDPOINT + '/tvsearch',
+	list : CHANTORU_ENDPOINT + '/list',
 	resv : CHANTORU_ENDPOINT + '/resv',
 };
 
 
-// API実行の共通部
-var sendApiCom = function (_method, _url, _args, ...restArgs) {
+/* 
+ * API実行の共通部
+ * @param
+ *   _method: ex. "GET"/"POST"
+ *   _url:    APIのエンドポイント
+ *   _url:    URLパラメータ、ヘッダー、POSTデータなど
+ *   _parser: APIによるレスポンスの違いを吸収するための関数
+ *   restArgs: EXPRESS
+ */
+var sendApiCom = function (_method, _url, _args, _parser, ...restArgs) {
 	var req = restArgs[0];
 	var res = restArgs[1];
 	clientMethods[_method](_url, _args)
-	.then(
-		function (val) { // なんらかのレスポンスがあった
+	.then(	
+		function (val) {
+			console.debug('then');
 			var data = val.data;
 			var response = val.response;
 			response.readable = true; // レスポンスを読み取るために必要
-			var strVal = data.toString('utf8');
-
-			// CHAN-TORUからのステータスコードをそのまま返す
-			//debugger;
-			try { // memo: 一覧取得はJSON, 予約はxmlで返ってくる
-				return res.status(response.statusCode).json(JSON.parse(strVal));
-				//return res.status(response.statusCode).send(strVal);
-			} catch (e) {
-				return res.status(response.statusCode).send(strVal);
-			}
+			
+			if (response.statusCode === 200) { // 200で返ってきたらこちらも200で返す
+				return res.status(200).send(_parser(data));
+			} else {                           // 200以外で返ってきたら400で返す
+				var decoded = data.toString('utf8');
+				console.error(decoded);
+				return res.status(400).send(decoded);
+			}	
 		})
 	.catch(    
-		function (error) { // レスポンスがなにもなかった
+		function (error) { // thenで例外発生時
+			console.debug('catch');
 			console.error(error);
 			return res.status(500).json({
 				errorCode: 'E001',
 				errorMsg:  'Error in connection to API server.'
 			});
-		}
-	);
-};
-
-var sendQuery = function (query, ...restArgs) {
-
+		});
 };
 
 // リクエストパラメータのチェック(リストにある文字列か？)
@@ -134,6 +140,8 @@ var date2Jst = function (date) {
 
 /* 
  * 番組表取得API
+ * @description
+ *   PostgreSQLのprogramsテーブルを取得
  * @param
  *   from:    '2020-01-02T05:00:00'  start_dateがちょうどを含む
  *   to:      '2020-01-09T05:00:00'  start_dateがちょうどを含まない
@@ -187,48 +195,33 @@ app.get(OWN_ENDPOINT.tvsearch, function(req, res){
 	})
 	.finally(() => {db.end();});
 
-	
-
 });
 
-
-// 番組予約API
-app.get(OWN_ENDPOINT.resv, function(req, res){
-	console.log('access: program reservation query.');
+/* 
+ * 番組予約一覧取得API
+ * @description
+ *   CHAN-TORUのlist APIを叩き、nasneの予約一覧を取得
+ * @params
+ *   
+ */
+app.get(OWN_ENDPOINT.schedule, function(req, res){
+	console.log('access: reservation list query.');
 
 	// CHAN-TORUに投げるクエリリクエストを生成
 	var query = req.query;
-	//console.log(query);
-	var param = {timestamp4p: Date.now()}; // 現在時刻で固定
-	console.log(param);
-	var form = {};
 
-	var defaultDate = date2Jst(new Date());
-	try {
-		form = {
-			op       : checkReqParamCand(['add', 'remove', 'modify'], query.op),
-			sid      : checkReqParamNumber('1024', query.sid), // チャンネルID
-			eid      : checkReqParamNumber('14965', query.eid), // 番組ID
-			category : checkReqParamCand(['1', '2'], query.category),
-			//date     : '2020-01-19T23:15:00+09:00', //checkReqParamDateFuck2(defaultDate, query.data), // 必須
-			duration : checkReqParamNumber('2700', query.duration), // 必須
-			//double   : 'on',
-			//title    : '%E3%82%B0%E3%83%83%E3%83%89%E3%83%BB%E3%83%95%E3%82%A1%E3%82%A4%E3%83%88%EF%BC%883%EF%BC%89%E3%80%8C%E7%96%91%E6%83%91%E3%81%AE%E3%83%AA%E3%82%B9%E3%83%88%E3%80%8D', // 任意
-			priority : '1',
-			//quality     : checkReqParamNumber('230', query.quality),
-			//condition   : checkReqParamCand(['w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7'], query.condition),
-			destination : checkReqParamCand(['HDD'], query.destination)
-		};
-	} catch (e) {
-		return res.status(400).send('Bad Request: ' + e + '<br>' + JSON.stringify(query));
-	}
-	console.log(form);
+	postParam = {
+		command: "schedule",
+		resp:    "json",
+		index:   0,
+		num:     0, // 0はおそらく全取得
+		type:    "manual",
+	};
 
 	var args = {
-		headers: require(HEADER_PATH),
-		//form: form,
-		data: form,
-		parameters: param,
+		headers: require(headerPath),
+		//data: {},
+		parameters: postParam,
 		// 以下効いていない？
 		requestConfig: {
 			timeout: TIMEOUT
@@ -239,11 +232,72 @@ app.get(OWN_ENDPOINT.resv, function(req, res){
 	};
 
 	// CHAN-TORU へのリクエスト実行
-	sendApiCom('POST', url.resv, args, ...arguments);
+	sendApiCom('POST', orgUrl.list, args, function (resData) {
+		//console.log(typeof resData); // object
+		var pretty = JSON.stringify(resData.list, null, " "); // インデントありで送信
+		return pretty;
+	}, ...arguments);
+});
+
+/* 
+ * 番組予約API
+ * @description
+ *   CHAN-TORUのlist APIを叩き、nasneに番組予約
+ */
+app.get(OWN_ENDPOINT.resv, function(req, res){
+	console.log('access: program reservation query.');
+
+	// CHAN-TORUに投げるクエリリクエストを生成
+	var query = req.query;
+	//console.log(query);
+	var postParam = {timestamp4p: Date.now()}; // 現在時刻で固定
+	console.log(postParam);
+	var postData = {};
+
+	var defaultDate = date2Jst(new Date());
+	try {
+		postData = {
+			op       : checkReqParamCand(['add', 'remove', 'modify'], query.op),
+			sid      : checkReqParamNumber('1024', query.sid), // チャンネルID
+			eid      : checkReqParamNumber('23251', query.eid), // 番組ID
+			category : checkReqParamCand(['1', '2'], query.category),
+			date     : '2020-01-01T21:50:00+09:00', //checkReqParamDateFuck2(defaultDate, query.data), // 必須
+			duration : checkReqParamNumber('1200', query.duration), // 必須
+			//double   : 'on',
+			//title    : '%E3%82%B0%E3%83%83%E3%83%89%E3%83%BB%E3%83%95%E3%82%A1%E3%82%A4%E3%83%88%EF%BC%883%EF%BC%89%E3%80%8C%E7%96%91%E6%83%91%E3%81%AE%E3%83%AA%E3%82%B9%E3%83%88%E3%80%8D', // 任意
+			priority : '1',
+			//quality     : checkReqParamNumber('230', query.quality),
+			//condition   : checkReqParamCand(['w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7'], query.condition),
+			destination : checkReqParamCand(['HDD'], query.destination)
+		};
+	} catch (e) {
+		return res.status(400).send('Bad Request: ' + e + '<br>' + JSON.stringify(query));
+	}
+	console.log(postData);
+
+	var args = {
+		headers: require(headerPath),
+		data: postData,
+		parameters: postParam,
+		// 以下効いていない？
+		requestConfig: {
+			timeout: TIMEOUT
+		},
+		responseConfig: {
+			timeout: TIMEOUT
+		}
+	};
+
+	// CHAN-TORU へのリクエスト実行
+	sendApiCom('POST', orgUrl.resv, args, function(resData) {
+		var pretty = JSON.stringify(resData.Result.$, null, " "); // インデントありで送信
+		console.debug(pretty);
+		return pretty;
+	}, ...arguments);	
 });
 
 
-/* listen()メソッドを実行して3001番ポートで待ち受け*/
+
 var server = app.listen(SERVER_PORT, function () {
 	var addr = this.address();
 	console.log('Node.js is listening to localhost:' + addr.port);
