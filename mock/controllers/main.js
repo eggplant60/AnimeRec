68a73a0fc4b4b2d6d19c18d61717355dc2fdb638
eggplant60 <nagoyan240@gmail.com>
2020-02-03 22:39:07 +0900

    [refactor] controller -> service へ処理を移管

--- mock/controllers/main.js
+++ mock/controllers/main.js
@@ -1,178 +1,137 @@
-angular.module('app')
-	.controller('AppCtrl', [
-		'$scope', 
-		'$log', 
-		'$timeout', 
-		'$filter', 
-		'$mdDialog',
-		'ApiService', 
-		'CommonService', 
-		AppCtrl])
-	.filter('extractDate', extractDate)
-	.filter('programTitle', programTitle)
-	.filter('episodeTitle', episodeTitle)
-	.filter('summaryShort', summaryShort);
+(function () {
+	'use strict';
 
-function AppCtrl($scope, $log, $timeout, $filter, $mdDialog, api, common) {
-	$log.debug('AppCtrl: start ---------------------');
+	angular.module('app')
+		.controller('AppCtrl', [
+			'$scope', 
+			'$log', 
+			'$timeout', 
+			'$filter', 
+			'$mdDialog',
+			'ApiService', 
+			'CommonService', 
+			AppCtrl]);
 
-	const genreId = '107100';
-	var vm = this;
-	vm.searchEngine='https://google.com/search?q=';
+	function AppCtrl($scope, $log, $timeout, $filter, $mdDialog, api, common) {
+		$log.debug('AppCtrl: start ---------------------');
 
-	/* 
-	 * 番組表取得
-	 */
-	const now = new Date();
-	const nColumns = 7;
-	const baseDate = common.roundDate(now);
-	let dateColumns = [baseDate];
-	for (let i=0; i < nColumns-1; i++) {
-		dateColumns.push(common.plus1day(dateColumns[i]));
-	}
+		const genreId = '107100';
+		const nColumns = 7;
 
-	vm.programList = dateColumns.map((element) => {
-		return {columnDate: element, rowPrograms: []};
-	});
+		var vm = this;
+		vm.searchEngine='https://google.com/search?q=';
 
-	for (let i in vm.programList) {
-		let thisDate = vm.programList[i].columnDate;
-		let params = {
-			from    : common.date2str(thisDate),
-			to      : common.date2str(common.plus1day(thisDate)),
-			genreId : genreId
-		};
-		api.tvsearch.get(params).$promise.then(
-			(data) => {
-				//$log.debug(data);
-				vm.programList[i].rowPrograms = data;
-			},
-			(err) => {
-				$log.error(err);
-			}
-		);
-	}
-	$log.debug('complete programs');
 
-	/* 
-	 * 予約一覧取得
-	 */
-	vm.schedule = [];
-	// api.schedule.get(api.default).$promise.then(
-	// 	(data) => {
-	// 		$log.debug(data);
-	// 		vm.schedule = data;
-	// 	}
-	// );
+		/* 
+		 * ビューの列数を早く確定させたいため、先に番組表の枠を作成する
+		 */
+		vm.programList = common.createEmptyProgramList(nColumns);
+		
+		/* 
+		 * DBの予約情報を更新依頼
+		 */
 
-	/*
-	 * 予約ボタン
-	 */
-	vm.onReserveButton = function(program) {
-		$log.debug('on reserve');
-		let param = {
-			area: program.area,
-			sid:  program.service_id,
-			pid:  program.program_id,
-			date: program.start_date,
-			duration: program.duration,
-		};
-		if (program.event_id) { // event_id をバッチで取得済みの場合
-			// @todo
-			param.eid = program.event_id;
-			api.reserve.get(param).$promise.then(
+		/* 
+		 * 番組表取得
+		 */
+		for (let i in vm.programList) {
+			let thisDate = vm.programList[i].columnDate;
+			let params = {
+				from    : common.date2str(thisDate),
+				to      : common.date2str(common.plus1day(thisDate)),
+				genreId : genreId
+			};
+			api.tvsearch.get(params).$promise.then(
 				(data) => {
-					$log.debug(data);
-					program.is_reserved = true;
-				},
-				(err) => {
-					$log.error(err);
-				}
-			);
-		} else {		
-			api.reserve2.get(param).$promise.then(
-				(data) => {
-					$log.debug(data);
-					program.is_reserved = true;
+					//$log.debug(data);
+					vm.programList[i].rowPrograms = data;
 				},
 				(err) => {
 					$log.error(err);
 				}
 			);
 		}
-	};
+		$log.debug('complete programs');
 
-	/* 
-	 * デバッグ用ダイアログ
-	 */
-	vm.openDebugDialog = function(program) {
-		$log.debug('dialog');
-		//$log.debug(program);
-		$mdDialog.show({
-			controller: DialogController,
-			templateUrl: 'partials/dialog.html',
-			parent: angular.element(document.body),
-			//targetEvent: ev,
-			clickOutsideToClose: true,
-			fullscreen: false,
-			locals: {
-				program: program
+		/* 
+		* 予約一覧取得
+		*/
+		//vm.schedule = [];
+		// api.schedule.get(api.default).$promise.then(
+		// 	(data) => {
+		// 		$log.debug(data);
+		// 		//vm.schedule = data;
+		// 		console.debug(vm.programList);
+		// 		common.updateIsReserved(vm.programList, data);
+		// 	}
+		// );
+
+		/*
+		* 予約ボタン
+		*/
+		vm.onReserveButton = function(program) {
+			$log.debug('on reserve');
+			let param = {
+				area: program.area,
+				sid:  program.service_id,
+				pid:  program.program_id,
+				date: program.start_date,
+				duration: program.duration,
+			};
+			if (program.event_id) { // event_id をバッチで取得済みの場合
+				// @todo
+				param.eid = program.event_id;
+				api.reserve.get(param).$promise.then(
+					(data) => {
+						$log.debug(data);
+						program.is_reserved = true;
+					},
+					(err) => {
+						$log.error(err);
+					}
+				);
+			} else {		
+				api.reserve2.get(param).$promise.then(
+					(data) => {
+						$log.debug(data);
+						program.is_reserved = true;
+					},
+					(err) => {
+						$log.error(err);
+					}
+				);
 			}
-		});
-		function DialogController($scope, $mdDialog, program) {
-			$scope.program = program;
-			$scope.hide = function() {
-				$mdDialog.hide();
-			};
-			$scope.cancel = function() {
-				$mdDialog.cancel();
-			};
-			$scope.answer = function(answer) {
-				$mdDialog.hide(answer);
-			};
-		}	
-	};
-}
+		};
 
-/* -------------- filter  --------------------------- */
-
-function extractDate () {
-	return function(value) {
-		return value.split('[')[0].trim();
-	};
-}
-
-function programTitle () {
-	return function(value) {
-		let tmp = value.replace(/　/g, ' ').replace(/＃/g, '#');
-		if (/^アニメ.+$/.test(tmp)) {
-			return tmp.split('#')[0].split('[')[0];
-		} else {
-			return tmp.split('「')[0].split('#')[0].split('[')[0];
-		}
-	};
-}
-
-function episodeTitle () {
-	return function(value) {
-		console.log(value);
-		let jp = value.match(/「(.+)」/);
-		console.log('ret: ' + jp);
-		let sharp = value.match(/(#[0-9]+)/);
-		console.log('ret: ' + sharp);
-		//console.log('ret: null' );
-		return '';
-	};
-}
-
-const summaryMaxLen = 70;
-
-function summaryShort () {
-	return function(value) {
-		if (value.length >= summaryMaxLen) {
-			return value.substr(0, summaryMaxLen) + '...';
-		} else {
-			return value;
-		}
-	};
-}
+		/* 
+		* デバッグ用ダイアログ
+		*/
+		vm.openDebugDialog = function(program) {
+			$log.debug('dialog');
+			//$log.debug(program);
+			$mdDialog.show({
+				controller: DialogController,
+				templateUrl: 'partials/dialog.html',
+				parent: angular.element(document.body),
+				//targetEvent: ev,
+				clickOutsideToClose: true,
+				fullscreen: false,
+				locals: {
+					program: program
+				}
+			});
+			function DialogController($scope, $mdDialog, program) {
+				$scope.program = program;
+				$scope.hide = function() {
+					$mdDialog.hide();
+				};
+				$scope.cancel = function() {
+					$mdDialog.cancel();
+				};
+				$scope.answer = function(answer) {
+					$mdDialog.hide(answer);
+				};
+			}	
+		};
+	}
+})();
--- mock/services/common.js
+++ mock/services/common.js
@@ -1,10 +1,9 @@
-(function () {
-	'use strict';
+//'use strict';
 
-	angular.module('app')
-			.service('CommonService', ['$log', '$filter', CommonService]);
+const summaryMaxLen = 70;
 
-	function CommonService($log, $filter) {
+angular.module('app')
+	.service('CommonService', ['$log', '$filter', function($log, $filter) {
 
 		this.roundDate = (date) => {
 			return new Date($filter('date')(date, 'yyyy-MM-dd 05:00:00'));
@@ -17,8 +16,79 @@
 		};
 
 		this.date2str = (date) => {
-			return $filter('date')(date, 'yyyy-MM-ddTHH:mm:ss')
+			return $filter('date')(date, 'yyyy-MM-ddTHH:mm:ss');
 		};
 
-	}
-})();
+		this.createEmptyProgramList = (nColumns) => {
+			let now = new Date();
+			let baseDate = this.roundDate(now);
+			let dateColumns = [baseDate];
+			for (let i=0; i < nColumns-1; i++) {
+				dateColumns.push(this.plus1day(dateColumns[i]));
+			}
+			return dateColumns.map((element) => {
+				return {
+					columnDate: element, 
+					rowPrograms: []
+				};
+			});
+		};
+
+		// var hex2dec = (hex) => parseInt(hex, 16).toString();
+		// this.updateIsReserved = (programs, schedule) => {
+		// 	schedule.forEach(element => {
+				
+		// 		for (let i=0; i<programs.length; i++) {					
+		// 			if (programs[i].columnDate <= element.startDateObj && 
+		// 				element.startDateObj < this.plus1day(programs[i].columnDate)) {
+		// 				console.debug('aaa----');
+		// 				console.debug(programs[i].columnDate, new Date(element.startDateObj));
+
+		// 			}
+		// 		}
+		// 		//hex2dec(element.eventId)
+		// 	});
+		// };
+
+	}])
+
+	.filter('extractDate', () => value => value.split('[')[0].trim())
+	.filter('programTitle', () => {
+		return (value) => {
+			let tmp = value.replace(/　/g, ' ').replace(/＃/g, '#');
+			if (/^アニメ.+$/.test(tmp)) {
+				return tmp.split('#')[0].split('[')[0];
+			} else {
+				return tmp.split('「')[0].split('#')[0].split('[')[0];
+			}
+		};
+	})
+	.filter('episodeTitle', () => {
+		return (value) => {
+			console.log(value);
+			let jp = value.match(/「(.+)」/);
+			console.log('ret: ' + jp);
+			let sharp = value.match(/(#[0-9]+)/);
+			console.log('ret: ' + sharp);
+			//console.log('ret: null' );
+			return '';
+		};
+	})
+	.filter('summaryShort', () => {
+		return (value) => {
+			if (value.length >= summaryMaxLen) {
+				return value.substr(0, summaryMaxLen) + '...';
+			} else {
+				return value;
+			}
+		};
+	});
+
+
+
+	
+
+
+
+
+
